970c07d15ae15f69e81938a8843a6660aeb98520
kyo1021 <kyo1021617@gmail.com>
2022-06-17 05:41:33 +0900

    seed.rb　画像ファイル設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 user1 = User.create(
-  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: File.open(Rails.root.join("./db/fixtures/icon1.jpg")));
-  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: File.open(Rails.root.join("./db/fixtures/shop1.jpg")),user_id:user1.id)
+  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: File.open(Rails.root.join('db/fixtures/icon1.jpg')));
+  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: File.open(Rails.root.join('db/fixtures/shop1.jpg')),user_id:user1.id)
 
-user2 = User.create(
-  username: "テストユーザー2", email: "test2@example.com", password: "passwordB", profile_image: File.open(Rails.root.join("./db/fixtures/icon2.jpg")));
-  Shop.create(title: "テストショップ2", body: "ふわふわオムレツのパスタです", image: File.open(Rails.root.join("./db/fixtures/shop2.jpg")),user_id:user2.id)
+# user2 = User.create(
+  # username: "テストユーザー2", email: "test2@example.com", password: "passwordB", profile_image: File.open(Rails.root.join("./db/fixtures/icon2.jpg")));
+  # Shop.create(title: "テストショップ2", body: "ふわふわオムレツのパスタです", image: File.open(Rails.root.join("./db/fixtures/shop2.jpg")),user_id:user2.id)
 
-user3 = User.create(
-  username: "テストユーザー3", email: "test3@example.com", password: "passwordC", profile_image: File.open(Rails.root.join("./db/fixtures/icon3.jpg")));
-  Shop.create(title: "テストショップ3", body: "これは美味しいステーキです", image: File.open(Rails.root.join("./db/fixtures/shop3.jpg")),user_id:user3.id)
+# user3 = User.create(
+  # username: "テストユーザー3", email: "test3@example.com", password: "passwordC", profile_image: File.open(Rails.root.join("./db/fixtures/icon3.jpg")));
+  # Shop.create(title: "テストショップ3", body: "これは美味しいステーキです", image: File.open(Rails.root.join("./db/fixtures/shop3.jpg")),user_id:user3.id)
