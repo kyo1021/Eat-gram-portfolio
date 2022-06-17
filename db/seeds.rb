567ead14e255a20cbdf8a2059417e05c5ce574d7
kyo1021 <kyo1021617@gmail.com>
2022-06-18 01:24:12 +0900

    seed.rb　画像ファイル設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 user1 = User.create(
-  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: File.open("./app/assets/images/icon1.jpg"));
-  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: File.open("./app/assets/images/shop1.jpg"),user_id:user1.id)
+  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: open("./app/assets/images/icon1.jpg"));
+  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: open("./app/assets/images/shop1.jpg"),user_id:user1.id)
 
 user2 = User.create(
-  username: "テストユーザー2", email: "test2@example.com", password: "passwordB", profile_image: File.open("./app/assets/images/icon2.jpg"));
-  Shop.create(title: "テストショップ2", body: "ふわふわオムレツのパスタです", image: File.open("./app/assets/images/shop2.jpg"),user_id:user2.id)
+  username: "テストユーザー2", email: "test2@example.com", password: "passwordB", profile_image: open("./app/assets/images/icon2.jpg"));
+  Shop.create(title: "テストショップ2", body: "ふわふわオムレツのパスタです", image: open("./app/assets/images/shop2.jpg"),user_id:user2.id)
 
 user3 = User.create(
-  username: "テストユーザー3", email: "test3@example.com", password: "passwordC", profile_image: File.open("./app/assets/images/icon3.jpg"));
-  Shop.create(title: "テストショップ3", body: "これは美味しいステーキです", image: File.open("./app/assets/images/shop3.jpg"),user_id:user3.id)
+  username: "テストユーザー3", email: "test3@example.com", password: "passwordC", profile_image: open("./app/assets/images/icon3.jpg"));
+  Shop.create(title: "テストショップ3", body: "これは美味しいステーキです", image: open("./app/assets/images/shop3.jpg"),user_id:user3.id)
