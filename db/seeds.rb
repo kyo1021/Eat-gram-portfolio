985a56a35e39e47f4bb621683327fa648242eeeb
kyo1021 <kyo1021617@gmail.com>
2022-06-17 20:00:40 +0900

    seed.rb　画像ファイル設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 user1 = User.create(
-  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: File.open(Rails.root.join('db/fixtures/icon1.jpg')));
-  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: File.open(Rails.root.join('db/fixtures/shop1.jpg')),user_id:user1.id)
+  username: "テストユーザー1", email: "test1@example.com", password: "passwordA", profile_image: File.open(Rails.root.join('./db/fixtures/icon1.jpg')));
+  Shop.create(title: "テストショップ1", body: "肉汁たっぷりハンバーグです", image: File.open(Rails.root.join('./db/fixtures/shop1.jpg')),user_id:user1.id)
 
 # user2 = User.create(
   # username: "テストユーザー2", email: "test2@example.com", password: "passwordB", profile_image: File.open(Rails.root.join("./db/fixtures/icon2.jpg")));
